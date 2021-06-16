80d97ad27174f2c81af85112a7910fae415457ce
Roger Fernandes <roger.osbourne@gmail.com>
2021-06-16 15:44:25 -0300

    first commit

---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$VERSION" ]; then
+  echo "VERSION should not be empty. Exiting with error."
+  exit 1
+fi
+
+config_file=k8s-$ENVIRONMENT-$VERSION.yml
+
+environment=$ENVIRONMENT version=$VERSION envsubst < deploy/$ENVIRONMENT/deployment.yml  > $config_file
+
+## apply deployment
+kubectl -n $NAMESPACE \
+  apply --record -s $K8S_CLUSTER \
+  --token=$K8S_TOKEN \
+  -f $config_file \
+  --insecure-skip-tls-verify || exit 1
+
+## check if the deploy was successful or not
+kubectl -n $NAMESPACE \
+  rollout status deployment/teams \
+  -s $K8S_CLUSTER \
+  --token=$K8S_TOKEN \
+  --insecure-skip-tls-verify || {
+## if failed then undo it
+kubectl -n $NAMESPACE \
+  rollout undo \
+  -s $K8S_CLUSTER \
+  --token=$K8S_TOKEN \
+  -f $config_file \
+  --insecure-skip-tls-verify; exit 1; }
