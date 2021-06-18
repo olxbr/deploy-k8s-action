db9bebec9b16f6197e867d8fac1cca2c4a6aea0b
Roger Fernandes <roger.osbourne@gmail.com>
2021-06-18 15:55:28 -0300

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,18 +7,16 @@ if [ -z "$VERSION" ]; then
   exit 1
 fi
 
-config_file=k8s-$ENVIRONMENT-$VERSION.yml
-
-sed "s/\${version}/$VERSION/" deploy/$ENVIRONMENT/deployment.yml > $config_file
-sed "s/\${environment}/$ENVIRONMENT/" deploy/$ENVIRONMENT/deployment.yml > $config_file
-
-cat $config_file
+sed -i '' "s/\${version}/$VERSION/" deploy/$ENVIRONMENT/*.yml
+sed -i '' "s/\${version}/$VERSION/" deploy/$ENVIRONMENT/*.yaml
+sed -i '' "s/\${environment}/$ENVIRONMENT/" deploy/$ENVIRONMENT/*.yml
+sed -i '' "s/\${environment}/$ENVIRONMENT/" deploy/$ENVIRONMENT/*.yaml
 
 ## apply deployment
 kubectl -n $NAMESPACE \
   apply --record -s $K8S_CLUSTER \
   --token=$K8S_TOKEN \
-  -f $config_file \
+  -f  deploy/$ENVIRONMENT --recursive \
   --insecure-skip-tls-verify || exit 1
 
 ## check if the deploy was successful or not
@@ -32,5 +30,5 @@ kubectl -n $NAMESPACE \
   rollout undo \
   -s $K8S_CLUSTER \
   --token=$K8S_TOKEN \
-  -f $config_file \
+  -f deploy/$ENVIRONMENT --recursive \
   --insecure-skip-tls-verify; exit 1; }
